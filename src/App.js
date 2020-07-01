a36220022f9237456a0aa32c0662c4011524ec33
Mubashir Imtiaz <mubashir.imtiaz123@gmail.com>
2020-07-01 20:58:39 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <h1 className="App-title">Todo App</h1>
+      <p style={{ color: "purple", fontSize: "1.1em" }}>
+        A Simple React Todo App!
+      </p>
       <TodosList />
     </div>
   );
